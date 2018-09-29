93903bf63bad16d0dff9817e20401eabb977c638
Romila <ms.romila@gmail.com>
2018-09-29 23:08:05 +0530

    Junit test case for Program Schedule

---
+++ test/sg/edu/nus/iss/phoenix/programschedule/dao/impl/test/ProgramScheduleDaoImplTest.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sg.edu.nus.iss.phoenix.programschedule.dao.impl.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import sg.edu.nus.iss.phoenix.core.dao.DBConstants;
+import sg.edu.nus.iss.phoenix.programschedule.entity.AnnualSchedule;
+import sg.edu.nus.iss.phoenix.programschedule.entity.WeeklySchedule;
+
+
+/**
+ *
+ * @author Hp
+ */
+public class ProgramScheduleDaoImplTest {
+    
+   Connection connection;
+   PreparedStatement stmt;
+    public ProgramScheduleDaoImplTest() {
+    }
+    
+    @Before
+    public void setUp() {
+        
+         try {
+            Class.forName(DBConstants.COM_MYSQL_JDBC_DRIVER);
+            try {
+                 this.connection = DriverManager.getConnection(DBConstants.dbUrl,
+                        DBConstants.dbUserName, DBConstants.dbPassword);
+                 assertNotNull(connection);
+            } catch (SQLException ex) {
+                Logger.getLogger(ProgramScheduleDaoImplTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+		
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ProgramScheduleDaoImplTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    @After
+    public void tearDown() {
+        this.connection = null;
+        assertNull(connection);
+    }
+
+    @Test
+    public void testLoadAllAnnualSchedule() {
+    
+        try {
+            
+            String sql = "SELECT * FROM `annual-schedule` ORDER BY `year` ASC; ";
+            if(connection==null){
+                fail();
+            }
+            stmt = connection.prepareStatement(sql);
+            if(stmt==null){
+                fail();
+            }
+            ResultSet result = stmt.executeQuery();
+            AnnualSchedule annualObj = new AnnualSchedule();
+            List<AnnualSchedule> annualSchd = new ArrayList<AnnualSchedule>();
+            assertNotNull(result);
+            
+            try {
+                while (result.next()){
+                    annualObj.setYear(result.getInt("year"));
+                    annualObj.setAssignedBy(result.getString("assingedBy"));
+                    annualSchd.add(annualObj);
+                }
+                assertNotNull(annualSchd);
+                assertFalse(annualSchd.isEmpty());
+                
+            } catch (SQLException ex) {
+                Logger.getLogger(ProgramScheduleDaoImplTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ProgramScheduleDaoImplTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+     }
+    
+    
+    @Test
+    public void testLoadWeeklySchedule() {
+    
+        try {
+            String year = "2018";
+            assertFalse(year.length()<4);
+            if(connection==null){
+                fail();
+            }
+            String sql = "SELECT * FROM `weekly-schedule` WHERE (year="+year+") ORDER BY `startDate` ASC";
+            stmt = connection.prepareStatement(sql);
+            if(stmt==null){
+                fail();
+            }
+            ResultSet result = stmt.executeQuery();
+            WeeklySchedule weekObj = new WeeklySchedule();
+            List<WeeklySchedule> weekSchd = new ArrayList<WeeklySchedule>();
+            assertNotNull(result);
+            try {
+                while (result.next()){
+                    weekObj.setYear(result.getInt("year"));
+                    weekObj.setStartDate(result.getDate("startDate"));
+                    weekSchd.add(weekObj);
+                }
+                assertFalse(weekSchd.isEmpty());
+                
+            } catch (SQLException ex) {
+                Logger.getLogger(ProgramScheduleDaoImplTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ProgramScheduleDaoImplTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+     }
+}
